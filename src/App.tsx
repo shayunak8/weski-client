468fb905061b5449cf41b947e53a037d6eafac64
shay <shayfriedman101@gmail.com>
2025-12-25 11:49:14 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from './components/navbar/nav-bar'
+
+const App: React.FC =() => {
+  return (
+    <div className='app'>
+      <NavBar />
+    </div>
+  )
+}
+
+export default App
